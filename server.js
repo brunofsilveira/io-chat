2c65c24c929f99de1f9dca3314b0306f660d0d29
Igor <igormjq@gmail.com>
2016-10-15 20:55:21 -0300

    Submit on 'enter' function added. Messages are now prepended not appended (go to top)

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var io = require('socket.io').listen(server);
 users = [];
 connections = [];
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 8000);
 console.log('Server RODANO na porta', "3000");
 
 app.get('/', function(req, res){
